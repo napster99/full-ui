8cddeede3ba4f9243adf5a36c58e0017a1af2021
vins <>
2019-08-02 15:27:19 +0800

    delete: 删除原有的非命令添加的组件

--- packages/tuya/search-box/index.js
+++
@@ -1,8 +0,0 @@
-import SearchBox from './src/search-box.vue';
-
-/* istanbul ignore next */
-SearchBox.install = function(Vue) {
-  Vue.component(SearchBox.name, SearchBox);
-};
-
-export default SearchBox;
--- packages/tuya/table/index.js
+++
@@ -1,8 +0,0 @@
-import TyTable from './src/table.vue';
-
-/* istanbul ignore next */
-TyTable.install = function(Vue) {
-  Vue.component(TyTable.name, TyTable);
-};
-
-export default TyTable;
