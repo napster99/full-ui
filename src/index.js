ac55ec25805db170a9ccce74eebdbcda8d5bfd15
寒汀 <zhangxj@tuya.com>
2019-08-07 16:24:50 +0800

    update

--- src/index.js
+++ src/index.js
@@ -208,6 +208,7 @@ const install = function(Vue, opts = {}) {
   Vue.prototype.$prompt = MessageBox.prompt;
   Vue.prototype.$notify = Notification;
   Vue.prototype.$message = Message;
+
 };
 
 /* istanbul ignore if */
